android-sms-gateway/client-go

[webhooks] add `sms:sent` event

smsgateway/webhooks/webhook.go
@@ -1,0 +1,39 @@
+package webhooks
+
+type EventType string
+
+const (
+	// Triggered when an SMS is received.
+	EventTypeSmsReceived EventType = "sms:received"
+	// Triggered when an SMS is sent.
+	EventTypeSmsSent EventType = "sms:sent"
+	// Triggered when the device pings the server.
+	EventTypeSystemPing EventType = "system:ping"
+)
+
+var allEventTypes = map[EventType]struct{}{
+	EventTypeSmsReceived: {},
+	EventTypeSmsSent:     {},
+	EventTypeSystemPing:  {},
+}
+
+// IsValid checks if the given event type is valid.
+//
+// e is the event type to be checked.
+// Returns true if the event type is valid, false otherwise.
+func IsValid(e EventType) bool {
+	_, ok := allEventTypes[e]
+	return ok
+}
+
+// A webhook configuration.
+type Webhook struct {
+	// The unique identifier of the webhook.
+	ID string `json:"id" validate:"max=36" example:"123e4567-e89b-12d3-a456-426614174000"`
+
+	// The URL the webhook will be sent to.
+	URL string `json:"url" validate:"required,http_url" example:"https://example.com/webhook"`
+
+	// The type of event the webhook is triggered for.
+	Event EventType `json:"event" validate:"required" example:"sms:received"`
+}

smsgateway/webhooks/webhook_test.go
@@ -1,0 +1,34 @@
+package webhooks_test
+
+import (
+	"testing"
+
+	"github.com/android-sms-gateway/client-go/smsgateway/webhooks"
+)
+
+// TestIsValid tests the IsValid function.
+func TestIsValid(t *testing.T) {
+	tests := []struct {
+		name string
+		e    webhooks.EventType
+		want bool
+	}{
+		{
+			name: "Valid event type",
+			e:    webhooks.EventTypeSmsReceived,
+			want: true,
+		},
+		{
+			name: "Invalid event type",
+			e:    "invalid:event",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := webhooks.IsValid(tt.e); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

hrenard/sms-gate-client-go

smsgateway/webhooks/webhook.go
@@ -1,0 +1,39 @@
+package webhooks
+
+type EventType string
+
+const (
+	// Triggered when an SMS is received.
+	EventTypeSmsReceived EventType = "sms:received"
+	// Triggered when an SMS is sent.
+	EventTypeSmsSent EventType = "sms:sent"
+	// Triggered when the device pings the server.
+	EventTypeSystemPing EventType = "system:ping"
+)
+
+var allEventTypes = map[EventType]struct{}{
+	EventTypeSmsReceived: {},
+	EventTypeSmsSent:     {},
+	EventTypeSystemPing:  {},
+}
+
+// IsValid checks if the given event type is valid.
+//
+// e is the event type to be checked.
+// Returns true if the event type is valid, false otherwise.
+func IsValidEventType(e EventType) bool {
+	_, ok := allEventTypes[e]
+	return ok
+}
+
+// A webhook configuration.
+type Webhook struct {
+	// The unique identifier of the webhook.
+	ID string `json:"id" validate:"max=36" example:"123e4567-e89b-12d3-a456-426614174000"`
+
+	// The URL the webhook will be sent to.
+	URL string `json:"url" validate:"required,http_url" example:"https://example.com/webhook"`
+
+	// The type of event the webhook is triggered for.
+	Event EventType `json:"event" validate:"required" example:"sms:received"`
+}

smsgateway/webhooks/webhook_test.go
@@ -1,0 +1,34 @@
+package webhooks_test
+
+import (
+	"testing"
+
+	"github.com/android-sms-gateway/client-go/smsgateway/webhooks"
+)
+
+// TestIsValidEventType tests the IsValidEventType function.
+func TestIsValidEventType(t *testing.T) {
+	tests := []struct {
+		name string
+		e    webhooks.EventType
+		want bool
+	}{
+		{
+			name: "Valid event type",
+			e:    webhooks.EventTypeSmsReceived,
+			want: true,
+		},
+		{
+			name: "Invalid event type",
+			e:    "invalid:event",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := webhooks.IsValidEventType(tt.e); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}