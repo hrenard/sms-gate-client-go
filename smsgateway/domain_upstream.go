64912d4ed118173a04511c3619139ee69a807c9c
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-12-31 07:54:27 +0700

    [messages] add messages export request

--- smsgateway/domain_upstream.go
+++ smsgateway/domain_upstream.go
@@ -8,6 +8,8 @@ const (
 	PushMessageEnqueued PushEventType = "MessageEnqueued"
 	// Webhooks are updated.
 	PushWebhooksUpdated PushEventType = "WebhooksUpdated"
+	// Messages export is requested.
+	PushMessagesExportRequested PushEventType = "MessagesExportRequested"
 )
 
 // A push notification.
@@ -15,7 +17,7 @@ type PushNotification struct {
 	// The token of the device that receives the notification.
 	Token string `json:"token" validate:"required" example:"PyDmBQZZXYmyxMwED8Fzy"`
 	// The type of event.
-	Event PushEventType `json:"event" validate:"omitempty,oneof=MessageEnqueued WebhooksUpdated" default:"MessageEnqueued" example:"MessageEnqueued"`
+	Event PushEventType `json:"event" validate:"omitempty,oneof=MessageEnqueued WebhooksUpdated MessagesExportRequested" default:"MessageEnqueued" example:"MessageEnqueued"`
 	// The additional data associated with the event.
 	Data map[string]string `json:"data"`
 }
--- smsgateway/requests.go
+++ smsgateway/requests.go
@@ -1,4 +1,16 @@
 package smsgateway
 
+import "time"
+
 // Push request
 type UpstreamPushRequest = []PushNotification
+
+// Messages export request
+type MessagesExportRequest struct {
+	// DeviceID is the ID of the device to export messages for.
+	DeviceID string `json:"deviceId" example:"PyDmBQZZXYmyxMwED8Fzy" validate:"required,max=21"`
+	// Since is the start of the time range to export.
+	Since time.Time `json:"since" example:"2024-01-01T00:00:00Z" validate:"required,ltefield=Until"`
+	// Until is the end of the time range to export.
+	Until time.Time `json:"until" example:"2024-01-01T23:59:59Z" validate:"required,gtefield=Since"`
+}
