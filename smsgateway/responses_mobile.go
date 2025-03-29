a93d369253a9fa963075b69b746b980c7e78c9f3
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-03-29 07:42:03 +0700

    [messages] add `createdAt` field

--- smsgateway/responses_mobile.go
+++ smsgateway/responses_mobile.go
@@ -21,3 +21,12 @@ type MobileUserCodeResponse struct {
 	Code       string    `json:"code" example:"123456"`                     // One-time code
 	ValidUntil time.Time `json:"validUntil" example:"2020-01-01T00:00:00Z"` // One-time code expiration time
 }
+
+// MobileMessage represents a Message in mobile response format
+type MobileMessage struct {
+	Message
+	CreatedAt time.Time `json:"createdAt" example:"2020-01-01T00:00:00Z"` // Message creation time
+}
+
+// MobileGetMessagesResponse represents a collection of messages for mobile clients
+type MobileGetMessagesResponse []MobileMessage
