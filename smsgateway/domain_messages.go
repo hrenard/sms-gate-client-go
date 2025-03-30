f4a962791ae7f6bdc99338e0de77eb28b67597bc
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-03-30 07:32:11 +0700

    [messages] add `priority` field

--- smsgateway/domain_messages.go
+++ smsgateway/domain_messages.go
@@ -5,12 +5,25 @@ import (
 	"time"
 )
 
+type (
+	// Processing state
+	ProcessingState string
+
+	// Message priority
+	MessagePriority int8
+)
+
 const (
 	ProcessingStatePending   ProcessingState = "Pending"   // Pending
 	ProcessingStateProcessed ProcessingState = "Processed" // Processed (received by device)
 	ProcessingStateSent      ProcessingState = "Sent"      // Sent
 	ProcessingStateDelivered ProcessingState = "Delivered" // Delivered
 	ProcessingStateFailed    ProcessingState = "Failed"    // Failed
+
+	PriorityMinimum         MessagePriority = -128
+	PriorityDefault         MessagePriority = 0
+	PriorityBypassThreshold MessagePriority = 100 // Threshold at which messages bypass limits and delays
+	PriorityMaximum         MessagePriority = 127
 )
 
 var allProcessStates = map[ProcessingState]struct{}{
@@ -23,12 +36,14 @@ var allProcessStates = map[ProcessingState]struct{}{
 
 // Message
 type Message struct {
-	ID                 string   `json:"id,omitempty" validate:"omitempty,max=36" example:"PyDmBQZZXYmyxMwED8Fzy"`                         // ID (if not set - will be generated)
-	Message            string   `json:"message" validate:"required,max=65535" example:"Hello World!"`                                     // Content
-	SimNumber          *uint8   `json:"simNumber,omitempty" validate:"omitempty,max=3" example:"1"`                                       // SIM card number (1-3), if not set - default SIM will be used
-	WithDeliveryReport *bool    `json:"withDeliveryReport,omitempty" example:"true"`                                                      // With delivery report
-	IsEncrypted        bool     `json:"isEncrypted,omitempty" example:"true"`                                                             // Is encrypted
-	PhoneNumbers       []string `json:"phoneNumbers" validate:"required,min=1,max=100,dive,required,min=1,max=128" example:"79990001234"` // Recipients (phone numbers)
+	ID           string   `json:"id,omitempty" validate:"omitempty,max=36" example:"PyDmBQZZXYmyxMwED8Fzy"`                         // ID (if not set - will be generated)
+	Message      string   `json:"message" validate:"required,max=65535" example:"Hello World!"`                                     // Content
+	PhoneNumbers []string `json:"phoneNumbers" validate:"required,min=1,max=100,dive,required,min=1,max=128" example:"79990001234"` // Recipients (phone numbers)
+	IsEncrypted  bool     `json:"isEncrypted,omitempty" example:"true"`                                                             // Is encrypted
+
+	SimNumber          *uint8          `json:"simNumber,omitempty" validate:"omitempty,max=3" example:"1"`                       // SIM card number (1-3), if not set - default SIM will be used
+	WithDeliveryReport *bool           `json:"withDeliveryReport,omitempty" example:"true"`                                      // With delivery report
+	Priority           MessagePriority `json:"priority,omitempty" validate:"omitempty,min=-128,max=127" example:"0" default:"0"` // Priority, messages with values greater than `99` will bypass limits and delays
 
 	TTL        *uint64    `json:"ttl,omitempty" validate:"omitempty,min=5" example:"86400"` // Time to live in seconds (conflicts with `validUntil`)
 	ValidUntil *time.Time `json:"validUntil,omitempty" example:"2020-01-01T00:00:00Z"`      // Valid until (conflicts with `ttl`)
--- smsgateway/types.go
+++ smsgateway/types.go
@@ -2,7 +2,4 @@ package smsgateway
 
 import "errors"
 
-// Processing state
-type ProcessingState string
-
 var ErrConflictFields = errors.New("conflict fields")
